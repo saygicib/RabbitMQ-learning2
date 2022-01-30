732c13448442371fc6b15b41e75b8b133b5a93c2
Buğrahan <bsaygici@hotmail.com>
2022-01-30 16:55:01 +0300

    Add project files.

---
+++ RabbitMQ-Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://uaujyisy:H3i_uCahwKuNuShYsQa6TdDHfY4Cr6rF@tiger.rmq.cloudamqp.com/uaujyisy");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(randomQueueName, "message-fanout", "", null);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName, false, consumer); //false=> Benim ack etmemi bekle.
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                channel.BasicAck(e.DeliveryTag, false);
+                Thread.Sleep(1000);
+                Console.WriteLine("Coming Message: " + message);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ-Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://uaujyisy:H3i_uCahwKuNuShYsQa6TdDHfY4Cr6rF@tiger.rmq.cloudamqp.com/uaujyisy");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("message-fanout", ExchangeType.Fanout, true);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                string message = $"{i}. Message";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("message-fanout","",null,messageBody);
+                Console.WriteLine($"Sent message : {message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
